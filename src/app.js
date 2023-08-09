b4dccf170acd997c5b2be072b1ba25aae085f22d
Namish KUCHAR KAL <namish.kuchar.kal@hexagon.com>
2023-08-09 16:20:57 +0530

    Added endpoints for GET User and Task for all and also for _id

--- src/app.js
+++ src/app.js
@@ -18,6 +18,24 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.status(200).send(users)
+    }).catch(error => {
+        res.status(500).send(error)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        res.status(200).send(user)
+    }).catch(error => {
+        res.status(500).send(error)
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -25,6 +43,27 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.status(200).send(tasks)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404).send(task)
+        }
+        res.send(task)
+    }).catch((error) => {
+        res.status(500).send(error)
     })
 })
 
