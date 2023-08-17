770f0974b99fb6b080ff0de27b4fac8c13825723
Namish KUCHAR KAL <namish.kuchar.kal@hexagon.com>
2023-08-17 12:39:10 +0530

    Generating Authentication tokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -42,8 +43,24 @@ const userSchema = new mongoose.Schema({
         save(value){
 
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = await jwt.sign({ _id: user._id.toString() }, 'thisisasecretkey')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    
+    return token
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
